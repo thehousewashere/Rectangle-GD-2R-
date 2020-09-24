b6e8db8abe7c41c879f6471c49cbdaa96e11919e
Jacob M. Thomas <jt1015776@francistuttle.edu>
2020-09-24 13:46:49 -0500

    Add project files.

---
+++ Rectangle/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1;
+            r1 = new Rectangle(1, 1, 1, 1);
+            r1.show();
+            r1.lengths(2);
+            r1.widths(2);
+            r1.postion(2, 2);
+            r1.show();
+            r1.area();
+            r1.perimeter();
+            r1.contains();
+            Console.WriteLine(r1.ToString());
+        }
+    }
+}
---
+++ Rectangle/rectangle.cs
@@ -0,0 +1,80 @@
+﻿using System;
+namespace Rectangle
+{
+    class Rectangle
+    {
+        private int x;
+        private int y;
+        private int length;
+        private int width;
+        public Rectangle()
+        {
+            int x = 0;
+            int y = 0;
+            int length = 0;
+            int width = 0;
+        }
+        public Rectangle(int x, int y, int length, int width)
+        {
+            this.x = x;
+            this.y = y;
+            this.length = length;
+            this.width = width;
+        }
+        public void show()
+        {
+            Console.WriteLine("Location: "+x+","+y+" |Length: "+length+" |Width: "+width);
+        }       
+        public void lengths(int l)
+        {
+            length = l;
+        }
+        public void widths(int w)
+        {
+            width = w;
+        }
+        public void postion(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public void area()
+        {
+            Console.WriteLine("Area: "+length * width+" ");
+        }
+        public void perimeter()
+        {
+            int per = (2 * length) + (2 * width);
+            Console.WriteLine("Perimeter: " + per + " ");
+        }
+        public bool contains()
+        {
+            int location = x * y;
+            int areas = length * width;
+
+            if (location == areas)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("The rectangle is contained");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                return true;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("The rectangle is not contained");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                return false;
+            }
+        }
+        public override string ToString()
+        {
+            string elam = ("(" + x + "," + y + "), " + length + ", " + width);
+            return elam;
+        }
+
+
+    }
+}
+
+
